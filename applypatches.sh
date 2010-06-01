3e83bf66edcada570ff39864e2c896e78bf6cdcd
Bradley Smith <bradleysmith93@gmail.com>
2010-06-01 22:33:52 +0930

    Add touchscreen patches to script

--- applypatches.sh
+++ applypatches.sh
@@ -7,3 +7,5 @@ patch -p1 < ../patches/linux-2.6.35-OMAP-DSS2-Add-ACX565AKM-Panel-Driver.patch
 patch -p1 < ../patches/linux-2.6.35-OMAP-RX51-Add-LCD-Panel-support.patch
 patch -p1 < ../patches/linux-2.6.35-OMAP-RX51-Add-vdds_sdi-supply-voltage-for-SDI.patch
 patch -p1 < ../patches/linux-2.6.35-OMAP-RX51-Add-Touch-Controller-in-SPI-board-info.patch
+patch -p1 < ../patches/linux-2.6.35-input-touchscreen-introduce-tsc2005-driver.patch
+patch -p1 < ../patches/linux-2.6.35-omap-rx-51-enable-tsc2005.patch
